ff1347cb92d1c6a8d5d440bb61f821d749860569
rifanid98 <adninsijawa.office@gmail.com>
2020-08-14 19:58:37 +0700

    fix product order color

--- src/Components/ProductOrder/index.js
+++ src/Components/ProductOrder/index.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import style from './style';
-import {color} from '../../Assets/Styles';
+import { color } from '../../Assets/Styles';
 import { apiUri } from '../../Utils/config';
 import { connect } from 'react-redux';
 import { setOrder } from '../../Redux/Action/order';
@@ -59,16 +59,16 @@ const ProductOrder = (props) => {
     <View style={style.container}>
       <Image
         style={style.productImage}
-        source={{ uri: `${apiUri.newImagePath}/${dataOrderDetail.image}`}}
+        source={{ uri: `${apiUri.newImagePath}/${dataOrderDetail.image}` }}
       />
       <View style={style.cardProduct}>
         <View style={style.productWrapper}>
           <View>
             <Text style={style.productTitle}>{dataOrderDetail.brand_name}</Text>
-            <View style={{flexDirection: 'row'}}>
-              <View style={{...style.productWrapper, marginRight: 20}}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ ...style.productWrapper, marginRight: 20 }}>
                 <Text style={style.fadeText}>Color: </Text>
-                <Text style={style.darkText}>{dataOrderDetail.color}</Text>
+                <Text style={{ ...style.darkText, ...style.productColor, backgroundColor: `${dataOrderDetail.color}`}}></Text>
               </View>
               <View style={style.productWrapper}>
                 <Text style={style.fadeText}>Size: </Text>
@@ -94,15 +94,15 @@ const ProductOrder = (props) => {
                   />
                 </TouchableOpacity>
                 : <TouchableOpacity
-                    style={style.counterButton}
-                    onPress={() => minusOrder(dataOrderDetail)}>
-                    {/* onPress={() => handleRemoveCount()}> */}
-                    <Ionicons
-                      name="remove-outline"
-                      size={20}
-                      color={color.default}
-                    />
-                  </TouchableOpacity>}
+                  style={style.counterButton}
+                  onPress={() => minusOrder(dataOrderDetail)}>
+                  {/* onPress={() => handleRemoveCount()}> */}
+                  <Ionicons
+                    name="remove-outline"
+                    size={20}
+                    color={color.default}
+                  />
+                </TouchableOpacity>}
               <Text style={style.counterText}>{dataOrderDetail.quantity}</Text>
               <TouchableOpacity
                 style={style.counterButton}
@@ -112,12 +112,12 @@ const ProductOrder = (props) => {
               </TouchableOpacity>
             </View>
           ) : (
-            <View style={style.counterContainer}>
-              <Text style={style.darkText}>
-                Units: {dataOrderDetail.quantity}
-              </Text>
-            </View>
-          )}
+              <View style={style.counterContainer}>
+                <Text style={style.darkText}>
+                  Units: {dataOrderDetail.quantity}
+                </Text>
+              </View>
+            )}
           <Text style={style.counterText}>{dataOrderDetail.sub_total}$</Text>
         </View>
       </View>
