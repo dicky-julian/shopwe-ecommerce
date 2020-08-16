102c1b80095e187202417f568d087fd00b9274d7
rifanid98 <adninsijawa.office@gmail.com>
2020-08-16 21:33:36 +0700

    update env

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-export const API_URL = `http://192.168.42.15:3000/shopwe/api/v1`;
-export const ORIGIN = `http://192.168.42.15:3000`;
+export const API_URL = `http://34.207.84.36/shopwe/api/v1`;
+export const ORIGIN = `http://34.207.84.36/`;
